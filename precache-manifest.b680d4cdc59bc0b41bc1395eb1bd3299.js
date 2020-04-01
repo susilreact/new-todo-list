706d34f62898d5f89123296d10601afeb3079a0d
susilreact <jenasusil@outlook.com>
2020-04-02 00:26:34 +0530

    Updates

---
+++ precache-manifest.b680d4cdc59bc0b41bc1395eb1bd3299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fca66d8747c54586508f007c971205",
+    "url": "/new-todo-list/index.html"
+  },
+  {
+    "revision": "b44917f9ade4cfc8a6f9",
+    "url": "/new-todo-list/static/css/main.54755469.chunk.css"
+  },
+  {
+    "revision": "bc09e3ae04e159f01b1e",
+    "url": "/new-todo-list/static/js/2.6e785cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-todo-list/static/js/2.6e785cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44917f9ade4cfc8a6f9",
+    "url": "/new-todo-list/static/js/main.ce72f75e.chunk.js"
+  },
+  {
+    "revision": "55d806994be0a9d314dd",
+    "url": "/new-todo-list/static/js/runtime-main.9511776d.js"
+  }
+]);
